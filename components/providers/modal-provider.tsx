3470de4ba5f41b4631a45cb1a1052218752fcf71
iamnevir <hiromansofaoi@gmail.com>
2023-10-21 15:18:32 +0700

    fix2

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import CreateServerModal from "../modals/create-server-modal";
 import InviteModal from "../modals/invite-modal";
 import EditServerModal from "../modals/edit-server-modal";
-import MembersModal from "../modals/members-modal";
+import { MembersModal } from "../modals/members-modal";
 import { LeaveServerModal } from "../modals/leave-server-modal";
 import { CreateChannelModal } from "../modals/create-channel-modal";
 import { DeleteServerModal } from "../modals/delete-server-modal";
